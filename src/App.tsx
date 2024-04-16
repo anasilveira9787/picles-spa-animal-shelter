f329bd72cf26893ba34e806d6f4004020fe4d858
anasilveira9787 <anacarolina.silveira@rocketseat.team>
2024-04-15 22:24:13 -0300

    create first component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
+import { Button } from './components/common/Button'
+import { ButtonVariant } from './components/common/Button/Button.constants'
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <button onClick={() => setCount((count) => count + 1)}>
-      count is {count}
-    </button>
+    <>
+    <Button variant={ButtonVariant.Default}>Quero adotar</Button>
+    <Button variant={ButtonVariant.Outlined}>Tenho um abrigo</Button>
+    </>
+    
   )
 }
